3b91dd710cad4642f8935e626f53b4610bcf43b8
Joakim Hägglund <zorlius@gmail.com>
2016-03-30 22:01:22 +0200

    Updated line endings, fixed broken hash

--- plugins/yr_plugin.rb
+++ plugins/yr_plugin.rb
@@ -7,7 +7,7 @@ class YrPlugin < BasePlugin
     @actions = ['yrväder']
   end
   def self.yrväder(msg)
-    cache = Dalli::Client.new('localhost:11211', :namespace => 'yr', :expires_in 15.minutes)
+    cache = Dalli::Client.new('localhost:11211', namespace: 'yr', expires_in: 15.minutes)
     stad_rxml = cache.get('uppsala')
     if stad_rxml.nil?
       stad_xml = open("http://www.yr.no/place/Sweden/Uppsala/Uppsala/forecast.xml").read
